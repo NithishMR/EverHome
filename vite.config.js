697954c10129b4b0b09b6f12053b256c8f5f159d
NithishMR <145840454+NithishMR@users.noreply.github.com>
2024-10-11 10:56:10 +0530

    Added build file

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-<<<<<<< HEAD
     outDir: "build", // Set the output directory to 'build'
   },
 });
-=======
-    outDir: 'build' // Set the output directory to 'build'
-  }
-})
->>>>>>> 56146e58e2ab194141b0af89ec456158bcaa4b2f
